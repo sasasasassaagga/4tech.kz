73acfddf62f930d1e9198a51c724826fe515f1bd
Adilkhan <144801358+sasasasassaagga@users.noreply.github.com>
2025-04-23 22:13:33 +0500

    Delete accounts directory

--- accounts/models.py
+++
@@ -1,5 +0,0 @@
-from django.db import models
-from django.contrib.auth.models import AbstractUser as DjangoUser
-
-class User(DjangoUser):
-    image = models.ImageField(upload_to='images/', default='images/default.jpg')
