538ea543e502f28eb667465d752c5a7897e63870
Adilkhan <144801358+sasasasassaagga@users.noreply.github.com>
2025-04-23 21:59:44 +0500

    Update urls.py

--- Ecommerce/urls.py
+++ Ecommerce/urls.py
@@ -18,10 +18,8 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from . import views
 
 urlpatterns = [
-    path('', views.index, name='base'),
     path('admin/', admin.site.urls),
     path('', include('products.urls')),
     path('accounts/', include('accounts.urls')),
